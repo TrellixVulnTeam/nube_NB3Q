ced09651613b192f6e4ff4e1349716ce63a8499f
Manuel Martin <manuelmf2001@gmail.com>
2021-01-31 13:36:27 +0100

    s

--- servidor/dwes_tarea_laravel/app/Alumno.php
+++ servidor/dwes_tarea_laravel/app/Alumno.php
@@ -5,7 +5,7 @@ namespace App;
 //use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Alumnos extends Model
+class Alumno extends Model
 {
     //use HasFactory;
 }
